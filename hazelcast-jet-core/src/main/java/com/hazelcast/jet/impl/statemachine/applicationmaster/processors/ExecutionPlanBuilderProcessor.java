e9a7e6fbae41e9ab1f4a44c91678403fd8b34520
Gillani0 <syd.gillani@gmail.com>
2017-07-13 13:51:00 +0200

    Fix null value in types array

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/statemachine/applicationmaster/processors/ExecutionPlanBuilderProcessor.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/statemachine/applicationmaster/processors/ExecutionPlanBuilderProcessor.java
@@ -182,9 +182,7 @@ public class ExecutionPlanBuilderProcessor implements ContainerPayLoadProcessor<
             Class[] argsClasses = new Class[args.length];
 
             for (Object obj : args) {
-                if (obj != null) {
-                    argsClasses[i++] = obj.getClass();
-                }
+                argsClasses[i++] = obj.getClass();
             }
 
             Constructor<ContainerProcessorFactory> resultConstructor = null;
@@ -193,8 +191,7 @@ public class ExecutionPlanBuilderProcessor implements ContainerPayLoadProcessor<
                 if (constructor.getParameterTypes().length == argsClasses.length) {
                     boolean valid = true;
                     for (int idx = 0; idx < argsClasses.length; idx++) {
-                        Class argsClass = argsClasses[idx];
-                        if (argsClass != null && !constructor.getParameterTypes()[idx].isAssignableFrom(argsClass)) {
+                        if (!constructor.getParameterTypes()[idx].isAssignableFrom(argsClasses[idx])) {
                             valid = false;
                             break;
                         }
